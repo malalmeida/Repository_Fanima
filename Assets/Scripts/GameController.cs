87eb9e7e862876305eaa69851fb9f1c412d2c961
mayraalmeida <mayraalmeida301@gmail.com>
2025-01-23 10:53:33 +0000

    Fixed bug
    Sentences reward did not appear after chapter 0 ended when sentences where done, but it appeared on the final board. (should appear in both)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -555,7 +555,6 @@ public class GameController : MonoBehaviour
   {
     if((SceneManager.GetActiveScene().name == "Geral"))
     {
-      geralScript.showWordsReward = true;
       geralScript.showSentencesReward = true;
     }
 
